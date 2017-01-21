45ea406b78f4d843c88522bebf0ff775507be668
verfass <verfass@nate.com>
2017-01-21 17:41:18 +0900

    3번째 커밋

--- app/src/main/java/kr/co/novacode/mycodinglayout/MainActivity.java
+++ app/src/main/java/kr/co/novacode/mycodinglayout/MainActivity.java
@@ -3,6 +3,8 @@ package kr.co.novacode.mycodinglayout;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -79,54 +81,51 @@ public class MainActivity extends AppCompatActivity {
         descTextView.setLayoutParams(params);
         topLayout.addView(descTextView);
 
-
-
-
-
-
-        /*
-37         <TextView
-38             android:id="@+id/textView02"
-39             android:layout_width="wrap_content"
-40             android:layout_height="wrap_content"
-41             android:text="@string/application_desc"
-42             android:textColor="@color/colorFontWhite"
-43             android:layout_gravity="center"
-44             android:layout_marginTop="10dp"
-45             android:textSize="30dp" />
-
-
-
-
-
-
-
-
-        // 바탕 레이아웃 설계
-        RelativeLayout mainLayout = new RelativeLayout(this);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT,
-                RelativeLayout.LayoutParams.MATCH_PARENT
+        // 하단 레이아웃 생성
+        LinearLayout bottomLayout = new LinearLayout(this);
+        params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
         );
-        mainLayout.setBackgroundColor(R.color.colorAccent);
-        mainLayout.setPadding(10, 10, 10, 10);
-        mainLayout.setLayoutParams(params);
+        params.weight = 2;
+        bottomLayout.setOrientation(LinearLayout.VERTICAL);
+        bottomLayout.setBackgroundColor(R.color.colorBackWhite);
+        bottomLayout.setLayoutParams(params);
+        mainLayout.addView(bottomLayout);
 
-        //타이틀 레이아웃
-        TextView titleTextView = new TextView(this);
-        params.width = RelativeLayout.LayoutParams.MATCH_PARENT;
-        params.height = RelativeLayout.LayoutParams.WRAP_CONTENT;
-        params.topMargin = 20;
-        titleTextView.setGravity(View.TEXT_ALIGNMENT_CENTER);
-        titleTextView.setText("식당 찾기 도우미");
-        titleTextView.setTextColor(Color.parseColor("#ffffffff"));
-        titleTextView.setTextSize(24);
+        // 하단 레이아웃 버튼01
+        Button button01 = new Button(this);
+        params = new LinearLayout.LayoutParams(
+                700,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.topMargin = 30;
+        params.gravity = Gravity.CENTER;
+        button01.setText("Create Account");
+        button01.setTextSize(25);
+        button01.setTextColor(R.color.colorBtnFontWhite);
+        button01.setBackgroundColor(R.color.colorBtnBlue);
+        button01.setPadding(15, 15, 15, 15);
+        button01.setAllCaps(false);
+        button01.setLayoutParams(params);
+        bottomLayout.addView(button01);
 
-        mainLayout.addView(titleTextView);
-
-
-                setContentView(mainLayout);
-        */
+        // 하단 레이아웃 버튼02
+        Button button02 = new Button(this);
+        params = new LinearLayout.LayoutParams(
+                700,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.topMargin = 30;
+        params.gravity = Gravity.CENTER;
+        button02.setText("Create Account");
+        button02.setTextSize(25);
+        button02.setTextColor(R.color.colorBtnFontBlue);
+        button02.setBackgroundColor(R.color.colorBtnWhite);
+        button02.setPadding(15, 15, 15, 15);
+        button02.setAllCaps(false);
+        button02.setLayoutParams(params);
+        bottomLayout.addView(button02);
 
     }
 }
